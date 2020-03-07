d07b596a69fa3735a76bb9edfb72acdc089d48be
Cook <vincentthomasetude@gmail.com>
2020-03-07 14:52:14 +0100

    Add noise on y axis

--- Introduction/Perlin Noise/sketch.js
+++ Introduction/Perlin Noise/sketch.js
@@ -1,4 +1,5 @@
 let xOffset = 0;
+let yOffset = 1000;
 
 function setup() { 
   createCanvas(400, 400);
@@ -9,7 +10,10 @@ function draw() {
 
   //let x = random(width);
   let x = map(noise(xOffset), 0, 1, 0, width);
-  xOffset += 0.01;
+  let y = map(noise(yOffset), 0, 1, 0, height);
 
-  ellipse(x, 200, 25, 25);
+  xOffset += 0.01;
+  yOffset += 0.01;
+
+  ellipse(x, y, 25, 25);
 }

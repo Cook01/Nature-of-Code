629082b5e7f069b700acd45fc0c966a3eb8b1d21
Cook <vincentthomasetude@gmail.com>
2020-03-07 09:44:30 +0100

    I.3 - noise() vs random()

--- Introduction/Perlin Noise/sketch.js
+++ Introduction/Perlin Noise/sketch.js
@@ -1,7 +1,15 @@
+let xOffset = 0;
+
 function setup() { 
   createCanvas(400, 400);
 } 
 
 function draw() { 
-  background(100);
+  background(50);
+
+  //let x = random(width);
+  let x = map(noise(xOffset), 0, 1, 0, width);
+  xOffset += 0.01;
+
+  ellipse(x, 200, 25, 25);
 }
